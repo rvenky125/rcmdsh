41913493eae1d8f29a77ba09adde920bf2a3a13b
venkatesh paithireddy <venkypaithireddy@gmail.com>
2026-09-03 14:43:58 +0530

    v0.3.6: PWA service worker no longer swallows real files

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { AdOverlay } from "./AdOverlay";
 import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    <AdOverlay />
+    {/* <AdOverlay /> temporarily disabled */}
   </StrictMode>,
 );
